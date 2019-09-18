c2d7abc59e39a8b8cf1603e7fb0136b001dde337
yangfan <18767120422@163.com>
2019-09-18 10:55:22 +0800

    fix: weex type changed

--- packages/rax-plugin-app/src/config/setEntry.js
+++ packages/rax-plugin-app/src/config/setEntry.js
@@ -4,13 +4,17 @@ const { hmrClient } = require('rax-compile-config');
 
 
 module.exports = (config, context, type) => {
-  process.env.RAX_TYPE = type;
   const { rootDir, command } = context;
   const isDev = command === 'dev';
 
   // SPA
   const appEntry = path.resolve(rootDir, 'src/app.js');
   const entryConfig = config.entry('index');
+
+  config.module.rule('appJSON')
+    .use('loader')
+    .tap(() => ({ type }));
+
   if (isDev) {
     entryConfig.add(hmrClient);
   }
