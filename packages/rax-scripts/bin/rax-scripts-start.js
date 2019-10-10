363f7a49079b509561cdbb46be3936bbe67cbbbc
fushen <fushen.jzw@alibaba-inc.com>
2019-10-10 20:04:43 +0800

    feat: support custom dev port

--- packages/rax-scripts/bin/rax-scripts-start.js
+++ packages/rax-scripts/bin/rax-scripts-start.js
@@ -7,10 +7,12 @@ const { dev } = require('scripts-core');
 
 program
   .option('--config <config>', 'use custom config')
+  .option('--port <port>', 'use custom port')
   .action((cmd) => {
     dev({
       args: {
         config: cmd.config,
+        port: cmd.port
       },
     });
   })
