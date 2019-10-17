571d51198f36785bb93a185d9bdb6e57fb8b8beb
水澜 <shuilan.cj@taobao.com>
2019-10-17 17:48:54 +0800

    refactor: command register for debug ssr app

--- packages/rax-scripts/bin/rax-scripts.js
+++ packages/rax-scripts/bin/rax-scripts.js
@@ -3,13 +3,48 @@
 
 
 const program = require('commander');
+const { dev, build } = require('scripts-core');
 const packageInfo = require('../package.json');
 
 program
   .version(packageInfo.version)
-  .usage('<command> [options]')
-  .command('build', 'Build project in production mode')
-  .command('start', 'Start a web server in development mode (hot-reload and inline-module-source-map is enable default)')
-  .command('lint', 'Lint for source  (only support component now)')
-  .command('test', 'Testing use jest(only support component now)')
-  .parse(process.argv);
+
+program
+  .command('start')
+  .description('Start a web server in development mode (hot-reload and inline-module-source-map is enable default)')
+  .option('--config <config>', 'use custom config')
+  .action((options) => {
+    dev({
+      args: {
+        config: options.config,
+      },
+    });
+  });
+
+program
+  .command('build')
+  .description('Build project in production mode')
+  .option('--config <config>', 'use custom config')
+  .action((options) => {
+    build({
+      args: {
+        config: options.config,
+      },
+    });
+  });
+
+program
+  .command('lint')
+  .description('Lint for source  (only support component now)')
+  .action(() => {
+    require('../src/lint')();
+  });
+
+program
+  .command('test')
+  .description('Testing use jest(only support component now)')
+  .action(() => {
+    require('../src/jest')();
+  });
+ 
+program.parse(process.argv);
