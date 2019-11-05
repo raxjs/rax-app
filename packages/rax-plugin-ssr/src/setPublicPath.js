bf59f7ab985b05399068a378d1c8ad8d6500c139
水澜 <shuilan.cj@taobao.com>
2019-11-05 18:01:34 +0800

    docs: update comments

--- packages/rax-plugin-ssr/src/setPublicPath.js
+++ packages/rax-plugin-ssr/src/setPublicPath.js
@@ -1,7 +1,6 @@
 const address = require('address');
 
-// 当页面和 assets 使用的不是同一个 server, 需将 publicPath 替换为绝对地址
-// 否则无论是模板中的资源还是异步加载的 bundle 都是以当前页面地址去加载的
+// ssr server may different from assets server, replace publicPath with an absolute address
 module.exports = (config) => {
   const publicPath = config.output.get('publicPath');
 
