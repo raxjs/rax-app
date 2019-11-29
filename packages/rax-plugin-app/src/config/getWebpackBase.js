b24dc57a11de125e3fb3c5a2465f1c0c85b92213
水澜 <shuilan.cj@taobao.com>
2019-11-29 16:18:47 +0800

    fix: white space

--- packages/rax-plugin-app/src/config/getWebpackBase.js
+++ packages/rax-plugin-app/src/config/getWebpackBase.js
@@ -1,9 +1,9 @@
 const webpack = require('webpack');
 const Chain = require('webpack-chain');
-const fs = require('fs-extra');	
+const fs = require('fs-extra');
 const path = require('path');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');	
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const { getBabelConfig, setBabelAlias } = require('rax-compile-config');
@@ -74,7 +74,7 @@ module.exports = (context, options = {}) => {
     .use(CaseSensitivePathsPlugin);
 
   if (fs.existsSync(path.resolve(rootDir, 'src/public'))) {
-    config.plugin('copyWebpackPlugin')	
+    config.plugin('copyWebpackPlugin')
       .use(CopyWebpackPlugin, [[{ from: 'src/public', to: 'public' }]]);
   }
 
