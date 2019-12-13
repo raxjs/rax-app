0ebd6b43be04ba1bf44c90e840249d4384614538
yacheng.sz <yacheng.sz@taobao.com>
2019-12-13 17:26:55 +0800

    chore: remove rax-scripts

--- packages/rax-scripts/src/babelTransformer.js
+++
@@ -1,8 +0,0 @@
-const { getBabelConfig } = require('rax-compile-config');
-
-const babelConfig = getBabelConfig({
-  styleSheet: true,
-});
-
-babelConfig.presets.push(require.resolve('@babel/preset-typescript'));
-module.exports = require('babel-jest').createTransformer(babelConfig);
