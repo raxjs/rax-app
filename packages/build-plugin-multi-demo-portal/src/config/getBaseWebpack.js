9441833c2dd69091782ac54e8b6b6cf3bc5eb5b2
lianmin <lianmin.slm@qq.com>
2019-12-18 22:17:32 +0800

    feat: add external portal page for rax component development

---
+++ packages/build-plugin-multi-demo-portal/src/config/getBaseWebpack.js
@@ -0,0 +1,113 @@
+const webpack = require("webpack");
+const Chain = require("webpack-chain");
+const { getBabelConfig, setBabelAlias } = require("rax-compile-config");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const { hmrClient } = require("rax-compile-config");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const generatePortalJs = require("./utils/generatePortalJs");
+const getDemos = require("./utils/getDemos");
+const getBuildTargets = require("./utils/getBuildTargets");
+
+const babelConfig = getBabelConfig({
+  styleSheet: true,
+  custom: {
+    ignore: ["**/**/*.d.ts"],
+  },
+});
+
+module.exports = context => {
+  const { rootDir, command, userConfig } = context;
+  const config = new Chain();
+
+  const demos = getDemos(rootDir);
+  const targets = getBuildTargets(userConfig);
+  const portalPath = generatePortalJs({
+    rootDir,
+    params: { demos, targets },
+  });
+
+  if (command === "start") {
+    config
+      .entry("portal")
+      .add(hmrClient)
+      .add(portalPath);
+  } else {
+    config.entry("portal").add(portalPath);
+  }
+
+  setBabelAlias(config);
+
+  config.target("web");
+  config.context(rootDir);
+
+  config.externals([
+    function(ctx, request, callback) {
+      if (request.indexOf("@weex-module") !== -1) {
+        return callback(null, `commonjs ${request}`);
+      }
+      callback();
+    },
+  ]);
+
+  config.resolve.extensions.merge([
+    ".js",
+    ".json",
+    ".jsx",
+    ".ts",
+    ".tsx",
+    ".html",
+  ]);
+
+  config.module
+    .rule("jsx")
+    .test(/\.(js|mjs|jsx)$/)
+    .use("babel")
+    .loader(require.resolve("babel-loader"))
+    .options(babelConfig);
+
+  config.module
+    .rule("tsx")
+    .test(/\.tsx?$/)
+    .use("babel")
+    .loader(require.resolve("babel-loader"))
+    .options(babelConfig)
+    .end()
+    .use("ts")
+    .loader(require.resolve("ts-loader"));
+
+  config.module
+    .rule("assets")
+    .test(/\.(svg|png|webp|jpe?g|gif)$/i)
+    .use("source")
+    .loader(require.resolve("image-source-loader"));
+
+  config.plugin("caseSensitivePaths").use(CaseSensitivePathsPlugin);
+
+  config.plugin("noError").use(webpack.NoEmitOnErrorsPlugin);
+
+  if (command === "start") {
+    config.mode("development");
+    config.devtool("inline-module-source-map");
+  } else if (command === "build") {
+    config.mode("production");
+
+    config.optimization
+      .minimizer("terser")
+      .use(TerserPlugin, [
+        {
+          terserOptions: {
+            output: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        },
+      ])
+      .end()
+      .minimizer("optimizeCSS")
+      .use(OptimizeCSSAssetsPlugin);
+  }
+
+  return config;
+};
---
+++ packages/build-plugin-multi-demo-portal/src/config/utils/getDemos.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const glob = require("glob");
+const fs = require("fs-extra");
+const hljs = require("highlight.js/lib/highlight");
+const htmlDecode = require("js-htmlencode").htmlDecode;
+const parseMd = require("../utils/parseMarkdown");
+
+hljs.registerLanguage(
+  "javascript",
+  require("highlight.js/lib/languages/javascript"),
+);
+
+module.exports = function(rootDir) {
+  const demos = [];
+  // read demos
+  glob.sync(path.resolve(rootDir, `demo/*.{js,jsx,md}`)).forEach(filePath => {
+    const name = filePath.substring(
+      filePath.lastIndexOf("/") + 1,
+      Math.max(filePath.indexOf(".js"), filePath.indexOf(".md")),
+    );
+    const demo = {
+      name,
+      filePath,
+      title: name,
+      order: 0,
+      js: "",
+      desc: "",
+    };
+
+    if (/\.md$/.test(filePath)) {
+      const result = parseMd(name, filePath);
+
+      demo.title = result.meta.title;
+      demo.js = result.js;
+      demo.order = result.meta.order;
+      demo.desc = result.body;
+    } else {
+      demo.js = fs.readFileSync(filePath, "utf-8");
+    }
+
+    demos.push({
+      ...demo,
+      // css,html 代码片段暂不处理
+      code: htmlDecode(hljs.highlight("javascript", demo.js).value),
+    });
+  });
+
+  return demos.sort((demo1, demo2) => {
+    return demo1.order > demo2.order ? 1 : -1;
+  });
+};
