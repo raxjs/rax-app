f33405a6ecd64defbfda34f3fd65e3dc9d685fc8
lianmin <406400939@qq.com>
2019-12-27 13:51:40 +0800

    feat: add ui component template

--- packages/rax-generator/src/config/index.js
+++ packages/rax-generator/src/config/index.js
@@ -49,6 +49,25 @@ const promptQuestion = [
     default: 'spa',
   },
   {
+    type: 'list',
+    name: 'componentType',
+    message: 'What\'s your component type?',
+    when(answers) {
+      return answers.projectType === 'component';
+    },
+    choices: [
+      {
+        name: 'Create lite component(A simple component which can support miniapp)',
+        value: 'lite',
+      },
+      {
+        name: 'Create ui component',
+        value: 'ui',
+      },
+    ],
+    default: 'lite',
+  },
+  {
     type: 'checkbox',
     name: 'projectTargets',
     validate(targets) {
@@ -93,7 +112,10 @@ const promptQuestion = [
         value: 'ssr',
         disabled: (answers) => {
           // Lite app is not support SSR
-          return answers.appType === 'lite' || !answers.projectTargets.includes('web');
+          return (
+            answers.appType === 'lite' ||
+            !answers.projectTargets.includes('web')
+          );
         },
       },
       {
