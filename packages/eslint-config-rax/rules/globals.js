0673de610ab80e3e874fed1e47ca8944b51fece0
元彦 <yuanyan.cao@alibaba-inc.com>
2020-01-17 16:06:16 +0800

    fix: add dir

---
+++ packages/eslint-config-rax/rules/globals.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'globals': {
+    '__weex_data__': 'readonly',
+    '__weex_options__': 'readonly',
+    '__weex_downgrade__': 'readonly',
+    '__weex_define__': 'readonly',
+    '__weex_require__': 'readonly',
+    'WXEnvironment': 'readonly',
+    'webkitRequestAnimationFrame': 'readonly',
+    'webkitCancelAnimationFrame': 'readonly',
+    'jasmine': 'readonly',
+    'React': 'readonly',
+  },
+};
---
+++ packages/eslint-config-rax/rules/react.js
@@ -0,0 +1,51 @@
+module.exports = {
+  'plugins': [
+    'react',
+  ],
+  'settings': {
+    'react': {
+      'pragma': 'createElement', // Pragma to use, default to "React"
+      'pragmaFrag': 'Fragment',
+    },
+  },
+  'rules': {
+    /**
+     * React & JSX
+     */
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/display-name': 'off',
+    'react/jsx-boolean-value': ['off', 'always'],
+    'react/jsx-no-bind': ['error', {
+      'allowArrowFunctions': true,
+    }],
+    'react/prefer-es6-class': 'error',
+    'react/jsx-curly-spacing': 'error',
+    'react/jsx-indent-props': ['error', 2], // 2 spaces indentation
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-tag-spacing': 'error',
+    'react/jsx-no-comment-textnodes': 'error',
+    'react/jsx-equals-spacing': 'error',
+    'react/jsx-handler-names': 'off',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-is-mounted': 'error',
+    'react/no-children-prop': 'error',
+    'react/no-did-mount-set-state': 'error',
+    'react/no-did-update-set-state': 'error',
+    'react/no-unknown-property': 'off',
+    'react/style-prop-object': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/self-closing-comp': 'error',
+    'react/sort-comp': ['off', {
+      'order': [
+        'lifecycle',
+        '/^on.+$/',
+        '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
+        'everything-else',
+        '/^render.+$/',
+        'render',
+      ],
+    }],
+  },
+};
