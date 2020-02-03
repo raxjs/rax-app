f116c0072ce8aa18573a0b061c0cdfc8f5e4a196
yacheng.sz <yacheng.sz@taobao.com>
2020-02-03 17:11:18 +0800

    feat: clear babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -7,16 +7,5 @@ module.exports = {
     ['@babel/preset-react', {
       'pragma': 'createElement'
     }]
-  ],
-  'plugins': [
-    '@babel/plugin-proposal-export-default-from',
-    ['@babel/plugin-proposal-class-properties', { 'loose': false }],
-    'babel-plugin-transform-jsx-stylesheet',
-    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-  ],
-  'ignore': [
-    'src/generator/templates',
-    '__mockc__',
-    'dist'
-  ],
+  ]
 };
