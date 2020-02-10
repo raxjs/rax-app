9b9fd07307922d0f040ca5f92c41bc3c1768a831
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-02-10 12:07:03 +0800

    fix: use lodash camelCase util

--- packages/build-plugin-rax-component/src/config/getUMDConfig.js
+++ packages/build-plugin-rax-component/src/config/getUMDConfig.js
@@ -1,18 +1,10 @@
 const getDistConfig = require('./getDistConfig');
-
-function toCamelCase(key) {
-  let strArray = key.split(/_|-/);
-  let humpStr = strArray[0];
-  for (let i = 1, l = strArray.length; i < l; i += 1) {
-    humpStr += strArray[i].slice(0, 1).toUpperCase() + strArray[i].slice(1);
-  }
-  return humpStr;
-}
+const _ = require('lodash');
 
 module.exports = (context, options) => {
   const config = getDistConfig(context, options);
   const {pkg} = context;
-  const pkgName = toCamelCase(pkg.name || '');
+  const pkgName = _.camelCase(pkg.name || '');
 
   config.output.library(pkgName);
   config.output.libraryTarget('umd');
