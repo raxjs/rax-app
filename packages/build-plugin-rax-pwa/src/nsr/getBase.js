52b02a20782bc3aa5f3cde10826e21313c69f037
boiawang <zhengwang2314@gmail.com>
2020-03-12 16:45:05 +0800

    feat: use rax-webpack-config replace build-plugin-rax-app

--- packages/build-plugin-rax-pwa/src/nsr/getBase.js
+++ packages/build-plugin-rax-pwa/src/nsr/getBase.js
@@ -1,16 +1,61 @@
-const getSSRBaseConfig = require('rax-ssr-config');
+const path = require('path');
+const getWebpackBase = require('rax-webpack-config');
+const getBabelConfig = require('rax-babel-config');
+const setUserConfig = require('./setUserConfig');
+const getEntryName = require('./getEntryName');
+const EntryPlugin = require('./entryPlugin');
 
 const EntryLoader = require.resolve('./entryLoader');
 
 module.exports = (context) => {
-  const config = getSSRBaseConfig({
-    ...context,
-    output: {
-      fileName: 'nsr/[name].js',
-      libraryTarget: 'umd'
-    },
-    entryLoader: EntryLoader,
+  const { userConfig, rootDir } = context;
+
+  const babelConfig = getBabelConfig({
+    styleSheet: true,
+    jsxToHtml: true,
   });
+
+  const config = getWebpackBase({
+    ...context,
+    babelConfig: babelConfig,
+  });
+
+  setUserConfig(config, context);
+
+  const { plugins, inlineStyle } = userConfig;
+  const isMultiPages = !!~plugins.indexOf('build-plugin-rax-multi-pages');
+  const appJSON = require(path.resolve(rootDir, 'src/app.json'));
+  const entries = appJSON.routes.map((route) => {
+    return {
+      name: getEntryName(route.path),
+      sourcePath: path.join(rootDir, 'src', route.source),
+      pagePath: route.path,
+    };
+  });
+
+  config.plugin('entryPlugin')
+    .use(EntryPlugin, [{
+      entries,
+      loader: EntryLoader,
+      isMultiPages: isMultiPages,
+      isInlineStyle: inlineStyle,
+      absoluteDocumentPath: path.join(rootDir, 'src/document/index.jsx'),
+      absoluteShellPath: path.join(rootDir, 'src/shell/index.jsx'),
+    }]);
+
+  config.output
+    .filename('nsr/[name].js')
+    .libraryTarget('umd');
+
+  if (!userConfig.inlineStyle) {
+    config.plugins.delete('minicss');
+    config.module.rules.delete('css');
+    config.module.rule('css')
+      .test(/\.css?$/)
+      .use('ignorecss')
+      .loader(require.resolve('./ignoreLoader'))
+      .end();
+  }
 
   return config;
 };
--- packages/build-plugin-rax-ssr/src/ssr/getBase.js
+++ packages/build-plugin-rax-ssr/src/ssr/getBase.js
@@ -1,27 +1,64 @@
-const getSSRBaseConfig = require('rax-ssr-config');
+const path = require('path');
+const getWebpackBase = require('rax-webpack-config');
+const getBabelConfig = require('rax-babel-config');
+const setUserConfig = require('./setUserConfig');
+const getEntryName = require('./getEntryName');
+const EntryPlugin = require('./entryPlugin');
 
 const EntryLoader = require.resolve('./entryLoader');
 
 // Can‘t clone webpack chain object, so generate a new chain and reset config
 module.exports = (context) => {
-  const config = getSSRBaseConfig({
-    ...context,
-    output: {
-      fileName: 'node/[name].js',
-      libraryTarget: 'commonjs2'
-    },
-    entryLoader: EntryLoader,
+  const { userConfig, rootDir } = context;
+
+  const babelConfig = getBabelConfig({
+    styleSheet: true,
+    jsxToHtml: true,
   });
+
+  const config = getWebpackBase({
+    ...context,
+    babelConfig: babelConfig,
+  });
+
+  setUserConfig(config, context);
 
   config.target('node');
 
-  ['jsx', 'tsx'].forEach(tag => {
-    config.module.rule(tag)
-      .use('platform')
-      .options({
-        platform: 'node',
-      });
+  const { plugins, inlineStyle } = userConfig;
+  const isMultiPages = !!~plugins.indexOf('build-plugin-rax-multi-pages');
+  const appJSON = require(path.resolve(rootDir, 'src/app.json'));
+  const entries = appJSON.routes.map((route) => {
+    return {
+      name: getEntryName(route.path),
+      sourcePath: path.join(rootDir, 'src', route.source),
+      pagePath: route.path,
+    };
   });
+
+  config.plugin('entryPlugin')
+    .use(EntryPlugin, [{
+      entries,
+      loader: EntryLoader,
+      isMultiPages: isMultiPages,
+      isInlineStyle: inlineStyle,
+      absoluteDocumentPath: path.join(rootDir, 'src/document/index.jsx'),
+      absoluteShellPath: path.join(rootDir, 'src/shell/index.jsx'),
+    }]);
+
+  config.output
+    .filename('node/[name].js')
+    .libraryTarget('commonjs2');
+
+  if (!userConfig.inlineStyle) {
+    config.plugins.delete('minicss');
+    config.module.rules.delete('css');
+    config.module.rule('css')
+      .test(/\.css?$/)
+      .use('ignorecss')
+      .loader(require.resolve('./ignoreLoader'))
+      .end();
+  }
 
   return config;
 };
