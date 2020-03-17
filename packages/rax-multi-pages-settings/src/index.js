452c2419c73977e7bc592234f65db887e6ce2627
yangfan <18767120422@163.com>
2020-03-17 19:14:41 +0800

    feat: update

--- packages/rax-multi-pages-settings/src/index.js
+++ packages/rax-multi-pages-settings/src/index.js
@@ -1,11 +1,14 @@
-
-
 const setConfig = require('./setConfig');
 const setDevLog = require('./setDevLog');
 const setDevServer = require('./setDevServer');
 
 module.exports = {
+  // Set MPA Webpack Config.
   setConfig,
+  // Set logs when dev server started.
+  // You will see "Multi pages development server at: xxx" in console.
   setDevLog,
+  // Set MPA dev server.
+  // You can preview you MPA pages though guide page.
   setDevServer
 };
