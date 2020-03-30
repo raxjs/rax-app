672908e94d63e0a5f378ab0d92517c4deb681d98
yacheng.sz <yacheng.sz@taobao.com>
2020-03-30 11:15:08 +0800

    feat: use  stylesheet-loader with inlineStyle false

--- packages/build-plugin-rax-app/src/config/getWebpackBase.js
+++ packages/build-plugin-rax-app/src/config/getWebpackBase.js
@@ -6,24 +6,24 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const { getBabelConfig, setBabelAlias } = require('rax-compile-config');
+const getWebpackBase = require('rax-webpack-config');
+const getBabelConfig = require('rax-babel-config');
 
 module.exports = (context, options = {}, target) => {
   const { rootDir, command } = context;
-  const config = new Chain();
 
   const babelConfig = getBabelConfig({
     styleSheet: true,
     ...options,
   });
 
+  const config = getWebpackBase({
+    ...context,
+    babelConfig: babelConfig,
+  });
+
   config.target('web');
   config.context(rootDir);
-
-  setBabelAlias(config);
-
-  config.resolve.extensions
-    .merge(['.js', '.json', '.jsx', '.html', '.ts', '.tsx']);
 
   config.resolve.alias
     .set('@core/app', 'universal-app-runtime')
@@ -41,21 +41,7 @@ module.exports = (context, options = {}, target) => {
     .use('loader')
     .loader(require.resolve('../loaders/AppConfigLoader'));
 
-  config.module.rule('jsx')
-    .test(/\.(js|mjs|jsx)$/)
-    .use('babel')
-    .loader(require.resolve('babel-loader'))
-    .options(babelConfig)
-    .end()
-    .use('platform')
-    .loader(require.resolve('rax-compile-config/src/platformLoader'));
-
   config.module.rule('tsx')
-    .test(/\.(ts|tsx)?$/)
-    .use('babel')
-    .loader(require.resolve('babel-loader'))
-    .options(babelConfig)
-    .end()
     .use('ts')
     .loader(require.resolve('ts-loader'))
     .options({
@@ -64,11 +50,6 @@ module.exports = (context, options = {}, target) => {
     .end()
     .use('platform')
     .loader(require.resolve('rax-compile-config/src/platformLoader'));
-
-  config.module.rule('assets')
-    .test(/\.(svg|png|webp|jpe?g|gif)$/i)
-    .use('source')
-    .loader(require.resolve('image-source-loader'));
 
   config.plugin('caseSensitivePaths')
     .use(CaseSensitivePathsPlugin);
@@ -91,30 +72,6 @@ module.exports = (context, options = {}, target) => {
       callback();
     },
   ]);
-
-  config.plugin('noError')
-    .use(webpack.NoEmitOnErrorsPlugin);
-
-  if (command === 'start') {
-    config.mode('development');
-    config.devtool('inline-module-source-map');
-  } else if (command === 'build') {
-    config.mode('production');
-
-    config.optimization
-      .minimizer('terser')
-      .use(TerserPlugin, [{
-        terserOptions: {
-          output: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }])
-      .end()
-      .minimizer('optimizeCSS')
-      .use(OptimizeCSSAssetsPlugin);
-  }
 
   return config;
 };
--- packages/build-plugin-rax-app/src/config/user/keys/inlineStyle.js
+++ packages/build-plugin-rax-app/src/config/user/keys/inlineStyle.js
@@ -122,8 +122,6 @@ function setCSSRule(configRule, context, value) {
         .use('postcss')
         .loader(require.resolve('postcss-loader'))
         .options(postcssConfig);
-    } else if (isInlineStandard) {
-      configInlineStyle(configRule);
     } else if (isNodeStandard) {
       // Do not generate CSS file, it will be built by web complier
       configRule
@@ -131,6 +129,7 @@ function setCSSRule(configRule, context, value) {
         .loader(require.resolve('../../../loaders/ignoreLoader'))
         .end();
     }
+    configInlineStyle(configRule);
   }
 }
 
