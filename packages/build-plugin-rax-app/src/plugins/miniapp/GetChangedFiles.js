502f03db3c1272604afc6db0e24cd2f3ab58b848
ChrisCindy <yxy0919@foxmail.com>
2020-05-09 21:05:54 +0800

    fix(jsx2mp): lint error

--- packages/build-plugin-rax-app/src/plugins/miniapp/GetChangedFiles.js
+++ packages/build-plugin-rax-app/src/plugins/miniapp/GetChangedFiles.js
@@ -3,11 +3,11 @@ module.exports = class GetChangedFilesPlugin {
     this.changedFiles = changedFiles;
   }
   apply(compiler) {
-    compiler.hooks.watchRun.tapAsync('GetChangedFiles', async (compiler, callback) => {
+    compiler.hooks.watchRun.tapAsync('GetChangedFiles', async(compiler, callback) => {
       const { watchFileSystem } = compiler;
       const watcher = watchFileSystem.watcher || watchFileSystem.wfs.watcher;
       this.changedFiles.splice(0, this.changedFiles.length, ...Object.keys(watcher.mtimes));
       callback();
-    })
+    });
   }
 };
