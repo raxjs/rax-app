21bcff096986b20bcc9c13b053d05084238fdfc2
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-05-21 10:13:42 +0800

    chore: remove es

--- examples/component/es/index.js
+++
@@ -1,9 +0,0 @@
-import { createElement } from 'rax';
-import View from 'rax-view';
-import Text from 'rax-text';
-
-var MyComponent = function () {
-  return createElement(View, null, createElement(Text, null, "Hello World!"));
-};
-
-export default MyComponent;
