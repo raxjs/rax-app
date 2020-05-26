f3d80ad184c6102b87797f25f5c9b06943de71fc
水澜 <shuilan.cj@taobao.com>
2020-05-26 17:48:22 +0800

    feat: support for sass

--- packages/build-plugin-rax-app/src/config/user/keys/inlineStyle.js
+++ packages/build-plugin-rax-app/src/config/user/keys/inlineStyle.js
@@ -26,6 +26,7 @@ module.exports = {
 
     setCSSRule(config.module.rule('css').test(/\.css?$/), context, value);
     setCSSRule(config.module.rule('less').test(/\.less?$/), context, value);
+    setCSSRule(config.module.rule('sass').test(/\.s[ac]ss?$/), context, value);
 
     if (inlineStandardList.includes(taskName) || value) {
       config.module.rule('less')
@@ -48,6 +49,10 @@ module.exports = {
           ignoreOrder: true
         }]);
     }
+
+    config.module.rule('sass')
+      .use('sass')
+      .loader(require.resolve('sass-loader'));
   },
 };
 
