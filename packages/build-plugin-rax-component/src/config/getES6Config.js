626e8b37591045cf7fce31460557539636babc5b
yacheng.sz <yacheng.sz@taobao.com>
2020-06-02 17:03:01 +0800

    feat: getES6Config.js

--- packages/build-plugin-rax-component/src/config/getES6Config.js
+++ packages/build-plugin-rax-component/src/config/getES6Config.js
@@ -3,10 +3,6 @@ const _ = require('lodash');
 
 module.exports = (context, options) => {
   const config = getDistConfig(context, {...options, isES6: true});
-  const {pkg} = context;
-  const pkgName = _.camelCase(pkg.name || '');
-
   config.output.filename('index-es6.js');
-
   return config;
 };
