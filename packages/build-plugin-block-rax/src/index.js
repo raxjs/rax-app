bf371a7b6121b95f686ffa6be96f1d2aeca5f493
yangfan <18767120422@163.com>
2020-06-04 17:39:42 +0800

    feat: update

--- packages/build-plugin-block-rax/src/index.js
+++ packages/build-plugin-block-rax/src/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const buildComponent = require('build-plugin-rax-component');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -7,12 +8,22 @@ module.exports = (api, options = {}) => {
   const { onGetWebpackConfig, context } = api;
   const { command, rootDir } = context;
 
+  // Use ice and rax 'demo/index' file as preview entry.
+  const demoDir = path.resolve(rootDir, 'demo');
+
+  if (
+    !fs.existsSync(demoDir) ||
+    !fs.readdirSync(demoDir).find(file => file.indexOf('index') > -1)
+  ) {
+    throw new Error('Rax block template need "demo/index" file to debug your block.');
+  }
+
   buildComponent(api, options);
 
   // Add build/index.html output
   if (command === 'build') {
     onGetWebpackConfig('component-build-web', (config) => {
-      config.entry('index').clear().add(path.resolve(rootDir, 'demo/index'));
+      config.entry('index').clear().add(path.resolve(demoDir, 'index'));
       config.plugin('html').use(HtmlWebpackPlugin);
       config.externals([]);
     });
