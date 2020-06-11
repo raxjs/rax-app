277cc2e67967f44394d82dba7f5b522380d56f38
ChrisCindy <yxy0919@foxmail.com>
2020-06-11 11:52:13 +0800

    chore(miniapp): rename function name

--- packages/build-plugin-rax-app/src/config/miniapp/filterNativePages.js
+++ packages/build-plugin-rax-app/src/config/miniapp/filterNativePages.js
@@ -4,13 +4,18 @@ const chokidar = require('chokidar');
 const { getDepPath } = require('./pathHelper');
 const targetPlatformMap = require('./targetPlatformMap');
 
-function isNativePages(filePath, target) {
+/**
+ * Judge whether the file is a native page according to the existence of the template file
+ * @param {string} filePath
+ * @param {string} target
+ */
+function isNativePage(filePath, target) {
   return existsSync(filePath + targetPlatformMap[target].tplExtension);
 }
 
 module.exports = (routes, { rootDir, target }) => {
   return routes.filter(({ source }) => {
     const pageEntry = getDepPath(rootDir, source);
-    return !isNativePages(pageEntry, target);
+    return !isNativePage(pageEntry, target);
   });
 };
