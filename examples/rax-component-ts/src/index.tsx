d2c0dbf12fa59fca355afb47278ea9bc5fe4f48c
imsobear <sobear.me@gmail.com>
2020-06-28 14:36:38 +0800

    fix: options not exist

--- examples/rax-component-ts/src/index.tsx
+++ examples/rax-component-ts/src/index.tsx
@@ -2,7 +2,11 @@ import { createElement } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
 
-const MyComponent = (props) => {
+interface IProps {
+  type: string;
+};
+
+const MyComponent = (props: IProps) => {
   return (
     <View>
       <Text>Hello World!</Text>
