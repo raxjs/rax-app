06a82943c528a09830e4176c83d6020c4acc177e
ChrisCindy <yxy0919@foxmail.com>
2020-06-29 15:53:28 +0800

    feat(miniapp): support add debug config

--- packages/rax-miniapp-runtime-webpack-plugin/src/generators/config.js
+++ packages/rax-miniapp-runtime-webpack-plugin/src/generators/config.js
@@ -1,16 +1,23 @@
 const addFileToCompilation = require('../utils/addFileToCompilation');
 
 module.exports = function(compilation, usingComponents, { target, command }) {
-  const config = {};
+  const config = {
+    usingComponents: {}
+  };
+
+  if (process.env.DEBUG === 'true') {
+    config.debug = true;
+  }
+
   Object.keys(usingComponents).forEach(name => {
-    config[name] = {
+    config.usingComponents[name] = {
       props: usingComponents[name].props,
       events: usingComponents[name].events
     };
   });
   addFileToCompilation(compilation, {
     filename: 'config.js',
-    content: `module.exports = ${JSON.stringify(usingComponents)}`,
+    content: `module.exports = ${JSON.stringify(config)}`,
     command,
     target,
   });
