7c91a8257f244ee8e2fb562c29708267d69e6618
水澜 <shuilan.cj@taobao.com>
2020-07-02 16:25:58 +0800

    feat: support boolean and string value

--- packages/build-plugin-rax-app/src/config/user/keys/sourceMap.js
+++ packages/build-plugin-rax-app/src/config/user/keys/sourceMap.js
@@ -1,13 +1,18 @@
 const { DOCUMENT } = require('../../../constants');
 
 module.exports = {
-  defaultValue: '', // Do not set defaultValue, so other plugin can get actual user config for sourcemap
-  validation: 'string',
+  validation: (val) => {
+    return typeof val === 'boolean' || typeof val === 'string';
+  },
   configWebpack: (config, value, context) => {
     const { command, taskName } = context;
     // Only valid in build mode and non-document task
-    if (command === 'build' && taskName !== DOCUMENT && value) {
-      config.devtool(value);
+    if (command === 'build' && taskName !== DOCUMENT) {
+      if (typeof value === 'boolean') {
+        value ? config.devtool('source-map') : config.devtool('none');
+      } else if (typeof value === 'string') {
+        config.devtool(value);
+      }
     }
   },
 };
