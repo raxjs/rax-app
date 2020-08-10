f11ecf880854675ff6b2bdae1b9a4a2faae2b3cd
fushen <fushen.jzw@alibaba-inc.com>
2020-08-10 14:50:27 +0800

    fix: rax-app resolve fail

--- packages/build-plugin-rax-app/src/config/miniapp/compile/getBase.js
+++ packages/build-plugin-rax-app/src/config/miniapp/compile/getBase.js
@@ -192,6 +192,11 @@ module.exports = (context, target, options = {}, onGetWebpackConfig) => {
       if (/^plugin\:\/\//.test(request)) {
         return callback(null, `commonjs2 ${request}`);
       }
+
+      // rax-app
+      if (/rax\-app/.test(request)) {
+        return callback(null, `commonjs2 ${request}`);
+      }
       callback();
     },
   ]);
