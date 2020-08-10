7f436b78cfe77b40b1e889227c12393d06f2b03f
NK <yxy0919@foxmail.com>
2020-08-10 10:59:16 +0800

    [RELEASE] miniapp @0806 (#397)
    * feat(miniapp): support dynamic slot
    
    * build(miniapp): publish new version
    
    * refactor(miniapp): use single package mechainsm (#396)
    
    * refactor: render method
    
    * chore: lint
    
    * chore: type
    
    * chore: lock eslint-plugin-react version
    
    * chore: resolve conflict
    
    * fix: handle icon path error (#393)
    
    * feat: jest config for rax (#384)
    
    * feat: jest config for rax
    
    * fix: css module mapper
    
    * chore: version
    
    * fix: testMatch rules
    
    Co-authored-by: 狒狒神 <350740249@qq.com>
    
    * chore(miniapp): add default usingComponents and usingPlugins value
    
    Co-authored-by: chenbin92 <chenbinbin.cb@alibaba-inc.com>
    Co-authored-by: ClarkXia <xiawenwu41@gmail.com>
    Co-authored-by: 逆葵 <xianyong.yxy@alibaba-inc.com>
    
    * feat: support set sock info (#400)
    
    * feat: support sock port
    
    * feat: support sock protocol
    
    * chore: remove empty line
    
    Co-authored-by: 狒狒神 <350740249@qq.com>
    Co-authored-by: chenbin92 <chenbinbin.cb@alibaba-inc.com>
    Co-authored-by: ClarkXia <xiawenwu41@gmail.com>

--- packages/rax-miniapp-runtime-webpack-plugin/src/generators/app.js
+++ packages/rax-miniapp-runtime-webpack-plugin/src/generators/app.js
@@ -1,9 +1,8 @@
 const { resolve, relative } = require('path');
 const { readFileSync } = require('fs-extra');
 const ejs = require('ejs');
-const { RawSource } = require('webpack-sources');
 const adapter = require('../adapter');
-const { VENDOR_CSS_FILE_NAME, MINIAPP } = require('../constants');
+const { MINIAPP } = require('../constants');
 const addFileToCompilation = require('../utils/addFileToCompilation');
 const getAssetPath = require('../utils/getAssetPath');
 const adjustCSS = require('../utils/adjustCSS');
@@ -18,13 +17,13 @@ function generateAppJS(
     'utf-8'
   );
   const appJsContent = ejs.render(appJsTmpl, {
-    init: `function init(window) {${commonAppJSFilePaths
+    init: `function init(window, document) {${commonAppJSFilePaths
       .map(
         filePath =>
           `require('${getAssetPath(
             relative(target, filePath),
             'app.js'
-          )}')(window)`
+          )}')(window, document)`
       )
       .join(';')}}`,
     isMiniApp: target === MINIAPP
@@ -49,24 +48,19 @@ function generateAppCSS(compilation, { target, command, rootDir }) {
     target,
     command,
   });
-  let appCssContent = '@import "./default";';
-  // If inlineStyle is set to false, css file will be extracted to vendor.css
-  const extractedAppCSSFilePath = `${target}/${VENDOR_CSS_FILE_NAME}`;
-  if (compilation.assets[extractedAppCSSFilePath]) {
-    compilation.assets[
-      `${extractedAppCSSFilePath}.${adapter[target].css}`
-    ] = new RawSource(
-      adjustCSS(compilation.assets[extractedAppCSSFilePath].source())
-    );
-    delete compilation.assets[extractedAppCSSFilePath];
-    appCssContent += `\n@import "./${VENDOR_CSS_FILE_NAME}";`;
-  }
 
   delete compilation.assets[`${target}/app.css`];
 
+  let content = '@import "./default";';
+
+  if (compilation.assets[`${target}/index.css`]) {
+    content += '@import "./index.css";';
+    delete compilation.assets[`${target}/index.css`];
+  }
+
   addFileToCompilation(compilation, {
     filename: `app.${adapter[target].css}`,
-    content: appCssContent,
+    content,
     target,
     command,
   });
