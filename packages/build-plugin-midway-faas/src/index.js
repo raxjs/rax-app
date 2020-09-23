c0e62fe00852f740d2bf856b9558aac5b3bd8643
shuilan.cj <shuilan.cj@taobao.com>
2020-09-23 11:30:30 +0800

    fix: lint

--- packages/build-plugin-midway-faas/src/index.js
+++ packages/build-plugin-midway-faas/src/index.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const fse = require("fs-extra");
-const { useExpressDevPack } = require("@midwayjs/faas-dev-pack");
+const path = require('path');
+const fse = require('fs-extra');
+const { useExpressDevPack } = require('@midwayjs/faas-dev-pack');
 
 module.exports = async ({
   context,
@@ -8,28 +8,28 @@ module.exports = async ({
 }) => {
   const { rootDir, command } = context;
 
-  const hasAPI = fse.existsSync(path.join(rootDir, "src/apis"));
+  const hasAPI = fse.existsSync(path.join(rootDir, 'src/apis'));
 
   const target = process.env.RAX_SSR ? 'ssr' : 'web';
 
   // Register FaaS Dev Server for API
   onGetWebpackConfig(target, (config) => {
-    if (command === "start" && hasAPI) {
-      config.devServer.set("writeToDisk", true);
+    if (command === 'start' && hasAPI) {
+      config.devServer.set('writeToDisk', true);
 
-      const originalDevServeBefore = config.devServer.get("before");
+      const originalDevServeBefore = config.devServer.get('before');
 
       config.merge({
         devServer: {
           before(app, server) {
-            if (typeof originalDevServeBefore === "function") {
+            if (typeof originalDevServeBefore === 'function') {
               originalDevServeBefore(app, server);
             }
 
             app.use(
               useExpressDevPack({
                 functionDir: rootDir,
-                sourceDir: "src/apis",
+                sourceDir: 'src/apis',
               })
             );
           },
