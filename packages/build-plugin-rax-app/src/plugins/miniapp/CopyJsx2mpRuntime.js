97bac39171374498f53ad8c85fa6dd88f5135e5b
逆葵 <xianyong.yxy@alibaba-inc.com>
2020-11-13 12:13:38 +0800

    fix(jsx2mp): rely jsx2mp-runtime miniprogram to copy file

--- packages/build-plugin-rax-app/src/plugins/miniapp/CopyJsx2mpRuntime.js
+++ packages/build-plugin-rax-app/src/plugins/miniapp/CopyJsx2mpRuntime.js
@@ -43,9 +43,7 @@ module.exports = class JSX2MPRuntimePlugin {
           runtimePackagePath = join(runtimePackageJSONPath, '..');
         }
 
-        const runtimeTargetPath = runtimePackageJSON.miniprogram && runtimePackageJSON.miniprogram[this.platform]
-          ? runtimePackageJSON.miniprogram[this.platform]
-          : runtimePackageJSON.main || 'index.js';
+        const runtimeTargetPath = `dist/jsx2mp-runtime.${this.platform}.esm.js`;
         const sourceFile = require.resolve(join(runtimePackagePath, runtimeTargetPath));
         const targetFile = join(this.outputPath, 'npm', runtime + '.js');
 
