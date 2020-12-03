088792e040416ca1308762a4fd8d64802c196607
fushen <fushen.jzw@alibaba-inc.com>
2020-12-03 15:34:31 +0800

    feat: add cancel task

--- packages/plugin-rax-app/src/utils/modifyTargets.js
+++ packages/plugin-rax-app/src/utils/modifyTargets.js
@@ -1,5 +1,30 @@
+const { KRAKEN, WEB, MINIAPP, WECHAT_MINIPROGRAM, BYTEDANCE_MICROAPP, WEEX } = require('../constants');
+
+const taskList = [
+  {
+    name: WEB,
+    children: ['pha', 'ssr'],
+  },
+  {
+    name: WEEX,
+  },
+  {
+    name: KRAKEN,
+  },
+];
+
+[MINIAPP, WECHAT_MINIPROGRAM, BYTEDANCE_MICROAPP].forEach((target) => {
+  taskList.push({
+    name: target,
+    children: [`rax-compiled-components-${target}`],
+  });
+});
+
 module.exports = (api) => {
-  const { context, modifyUserConfig } = api;
+  const { context, modifyUserConfig, cancelTask } = api;
+  const {
+    userConfig: { targets: originalTargets },
+  } = context;
   const targetsInCommand = context.commandArgs.targets;
   if (targetsInCommand) {
     const targets = targetsInCommand.split(',');
@@ -7,5 +32,16 @@ module.exports = (api) => {
       context.userConfig.targets = targets;
       return context.userConfig;
     });
+    if (originalTargets.length > targets.length) {
+      const removeTargets = originalTargets.filter((target) => !targets.includes(target));
+      taskList.forEach(({ name, children }) => {
+        if (removeTargets.includes(name)) {
+          cancelTask(name);
+          if (children) {
+            children.forEach((childTask) => cancelTask(childTask));
+          }
+        }
+      });
+    }
   }
 };
