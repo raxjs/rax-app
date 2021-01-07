dad51ad1088377f4d5b329e9e3273700e010608b
luhc228 <luhengchang228@gmail.com>
2021-01-07 19:49:48 +0800

    chore: remove store example code

--- examples/miniapp-subpackages/src/pages/About/module/index.jsx
+++ examples/miniapp-subpackages/src/pages/About/module/index.jsx
@@ -1,19 +1,16 @@
 import { createElement, useState } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
-import store from '../store';
 import styles from './index.module.css';
 
 import Logo from '@/components/Logo';
 
 export default function Home() {
   const [count, setCount] = useState(0);
-   const [state, dispatchers] = store.useModel('counter');
   return (
     <View className={styles.homeContainer}>
       <Logo uri="//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png" />
       { count }
-      {state.count}
       <Text
         onClick={() => {
           setCount(count + 1);
