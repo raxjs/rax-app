bcc488d7d1fc33bed8e8cede07d4147596be0d0a
fushen <fushen.jzw@alibaba-inc.com>
2021-02-02 16:05:48 +0800

    chore: optimize tag name

--- packages/plugin-rax-store/src/index.ts
+++ packages/plugin-rax-store/src/index.ts
@@ -23,7 +23,7 @@ export default async (api) => {
     applyMethod('addDisableRuntimePlugin', pluginName);
     return;
   }
-  process.env.enabled_store = 'true';
+  process.env.STORE_ENABLED = 'true';
 
   const appStoreFilePath = formatPath(getAppStorePath({ srcPath, projectType }));
   const existsAppStoreFile = fse.pathExistsSync(appStoreFilePath);
