32388d48ca8336751d39330a37cdf2463b07e4a0
fushen.jzw <fushen.jzw@alibaba-inc.com>
2021-03-05 02:00:30 +0800

    chore: add comment

--- packages/plugin-rax-web/src/Plugins/DocumentPlugin.ts
+++ packages/plugin-rax-web/src/Plugins/DocumentPlugin.ts
@@ -34,13 +34,15 @@ export default class DocumentPlugin {
     compiler.hooks.emit.tapAsync(PLUGIN_NAME, async (compilation, callback) => {
       const enableListen = getEnableStatus();
       /**
-       * 1. Exist local builder, before local builder compiler created (it's more earlier than this hook) enable listen will be true,
+       * 1. Exist local builder, before local builder compiler created (it's more earlier than this hook) enableListen will be true,
        *  so it will resolve the first listen resolve, and then hot reload:
        *  - Something changed that will emit web and local builder all restart, it will be ok as the first one:
        *    - web builder is faster than local builder, enable status has been true when restart(before local builder compiler created), so it will wait local builder emits assets
        *    - local builder is faster than web builder, localBuildTask is ready to resolve when web finished
        *  - Something changed that will only emit web changed:
        *    - the enableListen status has been false when latest emitted, so it will use the last time cache assets as html content
+       *
+       * 2. There is no local builder, enableListen will be false, everything will be ok, cacheAssets default value is {}
        */
       if (enableListen) {
         localBuildTask.then(emitAssets);
