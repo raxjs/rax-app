cdd5317fa96c9fbceb890c398769af167063af0a
fushen <fushen.jzw@alibaba-inc.com>
2021-03-10 20:32:15 +0800

    feat: support transform app.js

--- packages/codemod/src/transforms/app/project.ts
+++ packages/codemod/src/transforms/app/project.ts
@@ -3,11 +3,11 @@ import transformBuildConfig from './build.json';
 import transformPkgJSON from './package.json';
 import transformAppJSON from './app.json';
 import transformEslint from './eslint';
+import transformAppEntry from './transformAppEntry';
 
 export default function (fileInfo, api, options) {
-  console.log('options===>', options);
   let source;
-  switch (path.basename(fileInfo.path)) {
+  switch (fileInfo.path) {
     case 'package.json':
       source = transformPkgJSON(fileInfo);
       break;
@@ -17,11 +17,27 @@ export default function (fileInfo, api, options) {
     case 'src/app.json':
       source = transformAppJSON(fileInfo);
       break;
+    case 'src/app.js':
+    case 'src/app.jsx':
+    case 'src/app.ts':
+    case 'src/app.tsx':
+      source = transformAppEntry(fileInfo, api);
+      break;
     case '.eslintrc.js':
       source = transformEslint();
       break;
     default:
-      source = fileInfo.source;
+      if (options.customBuildJSON) {
+        const absolutePath = path.resolve(process.cwd(), fileInfo.path);
+        if (Array.isArray(options.customBuildJSON) && options.customBuildJSON.includes(absolutePath)) {
+          source = transformBuildConfig(fileInfo);
+        } else if (options.customBuildJSON === absolutePath) {
+          source = transformBuildConfig(fileInfo);
+        }
+      } else {
+        source = fileInfo.source;
+      }
   }
+
   return source;
 }
