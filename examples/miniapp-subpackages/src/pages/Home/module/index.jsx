b3bd7c59332cd73c2aec1ef5549573e1854f8681
狒狒神 <fushen.jzw@alibaba-inc.com>
2021-03-23 15:29:45 +0800

    chore: reduce miniapp size with subPackages (#667)

--- examples/miniapp-subpackages/src/pages/Home/module/index.jsx
+++ examples/miniapp-subpackages/src/pages/Home/module/index.jsx
@@ -1,6 +1,7 @@
 import { createElement } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
+import { isMiniApp } from 'universal-env';
 
 import styles from './index.module.css';
 
@@ -11,9 +12,15 @@ export default function Home() {
     <View className={styles.homeContainer}>
       <Logo uri="//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png" />
       <Text className={styles.homeTitle} onClick={() => {
-        my.navigateTo({
-          url: '/pages/About/module/index'
-        });
+        if (isMiniApp) {
+          my.navigateTo({
+            url: '/pages/About/module/index'
+          });
+        } else {
+          wx.navigateTo({
+            url: '/pages/About/module/index'
+          });
+        }
       }}>点我去到子包页面</Text>
       <Text className={styles.homeInfo}>More information about Rax</Text>
       <Text className={styles.homeInfo}>Visit https://rax.js.org</Text>
