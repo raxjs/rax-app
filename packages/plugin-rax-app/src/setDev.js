d8cadc02a48135958f7898b95b15b14c688f9da4
狒狒神 <fushen.jzw@alibaba-inc.com>
2021-03-25 14:39:50 +0800

    chore: remove print local url in console (#681)
    * chore: remove print local url in console
    
    * chore: auto open ip address
    
    * fix: print url

--- packages/plugin-rax-app/src/setDev.js
+++ packages/plugin-rax-app/src/setDev.js
@@ -86,7 +86,7 @@ module.exports = function (api) {
     logWebpackConfig(configs);
   });
 
-  onHook('after.start.compile', async ({ urls, stats }) => {
+  onHook('after.start.compile', async ({ stats }) => {
     const statsJson = stats.toJson({
       all: false,
       errors: true,
@@ -168,21 +168,18 @@ module.exports = function (api) {
             const entryPath = webMpa ? `${entryKey}${web.ssr ? '' : '.html'}` : '';
             const lanUrl = `${urlPrefix}/${entryPath}`;
             devInfo.urls.web.push(lanUrl);
-            showLocalUrl && console.log(`  ${chalk.underline.white(`${urls.localUrlForBrowser}${entryPath}`)}`);
             console.log(`  ${chalk.underline.white(lanUrl)}`);
-            console.log();
             if (shouldOpenBrowser && openEntries.includes(entryKey)) {
-              openBrowser(`${urls.localUrlForBrowser}${entryPath}`);
+              openBrowser(lanUrl);
             }
           });
         } else {
           devInfo.urls.web.push(`${urlPrefix}/`);
-          console.log(`  ${chalk.underline.white(`${urls.localUrlForBrowser}`)}`);
           console.log(`  ${chalk.underline.white(`${urlPrefix}/`)}`);
           console.log();
 
           if (shouldOpenBrowser) {
-            openBrowser(`${urls.localUrlForBrowser}`);
+            openBrowser(`${urlPrefix}/`);
           }
         }
       }
