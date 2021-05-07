1fe9f30fd7be4fac81f8a2b088091c8358a3baa2
fushen <fushen.jzw@alibaba-inc.com>
2021-05-07 20:00:23 +0800

    chore: format code

--- packages/plugin-rax-pha/src/plugins/AppToManifestPlugin.js
+++ packages/plugin-rax-pha/src/plugins/AppToManifestPlugin.js
@@ -16,23 +16,17 @@ module.exports = class {
   }
 
   apply(compiler) {
-    const {
-      api,
-      builtInLibrary = [],
-      appWorkerPath,
-    } = this.options;
+    const { api, builtInLibrary = [], appWorkerPath } = this.options;
 
     const { context, getValue } = api;
     const { command, userConfig = {} } = context;
     const { inlineStyle, web = {} } = userConfig;
-    const { pha: { template: isTemplate = true } } = web;
+    const {
+      pha: { template: isTemplate = true },
+    } = web;
     const isStart = command === 'start';
 
-    let {
-      cdnPrefix = '',
-      pagePrefix = '',
-      pageSuffix,
-    } = this.options;
+    let { cdnPrefix = '', pagePrefix = '', pageSuffix } = this.options;
     if (isStart) {
       cdnPrefix = `${getValue('devUrlPrefix')}/`;
       pagePrefix = cdnPrefix;
@@ -52,18 +46,24 @@ module.exports = class {
         }
       }
 
-      manifestJSON.scripts = [...builtInLibrary.map(url => `<script crossorigin="anonymous" src="${url}"></script>`), ...(manifestJSON.scripts || [])];
+      manifestJSON.scripts = [
+        ...builtInLibrary.map((url) => `<script crossorigin="anonymous" src="${url}"></script>`),
+        ...(manifestJSON.scripts || []),
+      ];
 
       // if has tabBar, do not generate multiple manifest.json
       if (manifestJSON.tab_bar) {
-        manifestJSON = setRealUrlToManifest({
-          urlPrefix: pagePrefix,
-          urlSuffix: pageSuffix,
-          cdnPrefix,
-          isTemplate,
-          inlineStyle,
-          api,
-        }, manifestJSON);
+        manifestJSON = setRealUrlToManifest(
+          {
+            urlPrefix: pagePrefix,
+            urlSuffix: pageSuffix,
+            cdnPrefix,
+            isTemplate,
+            inlineStyle,
+            api,
+          },
+          manifestJSON,
+        );
 
         compilation.assets['manifest.json'] = new RawSource(JSON.stringify(manifestJSON, null, 2));
 
@@ -73,42 +73,47 @@ module.exports = class {
           target: 'web',
           appJsonContent: appConfig,
         });
-        entries.filter(({ __frameIndex, __pageHeader, __tabBar }) => {
-          if ((typeof __frameIndex !== 'undefined' && __frameIndex !== 0) || __pageHeader || __tabBar) {
-            return false;
-          }
-          return true;
-        }).forEach(({ source, entryName, __frameIndex }) => {
-          let copyManifestJSON = cloneDeep(manifestJSON);
-          copyManifestJSON.pages = copyManifestJSON.pages.filter((page) => {
-            // has frames
-            if (__frameIndex === 0) {
-              return !!(page.frames && page.frames[0] && page.frames[0].source === source);
-            } else {
-              return page.source === source;
+        entries
+          .filter(({ __frameIndex, __pageHeader, __tabBar }) => {
+            if ((typeof __frameIndex !== 'undefined' && __frameIndex !== 0) || __pageHeader || __tabBar) {
+              return false;
             }
+            return true;
+          })
+          .forEach(({ source, entryName, __frameIndex }) => {
+            let copyManifestJSON = cloneDeep(manifestJSON);
+            copyManifestJSON.pages = copyManifestJSON.pages.filter((page) => {
+              // has frames
+              if (__frameIndex === 0) {
+                return !!(page.frames && page.frames[0] && page.frames[0].source === source);
+              } else {
+                return page.source === source;
+              }
+            });
+
+            const { pages } = copyManifestJSON;
+            // take out the page data prefetch and assign it to the root node
+            if (pages && pages[0] && pages[0].data_prefetch) {
+              copyManifestJSON.data_prefetch = pages[0].data_prefetch;
+              delete pages[0].data_prefetch;
+            }
+
+            copyManifestJSON = setRealUrlToManifest(
+              {
+                urlPrefix: pagePrefix,
+                urlSuffix: pageSuffix,
+                cdnPrefix,
+                isTemplate,
+                inlineStyle,
+                api,
+              },
+              copyManifestJSON,
+            );
+
+            compilation.assets[`${entryName}-manifest.json`] = new RawSource(JSON.stringify(copyManifestJSON, null, 2));
+
+            devUrls.push(`${cdnPrefix}${entryName}-manifest.json?pha=true`);
           });
-
-          const { pages } = copyManifestJSON;
-          // take out the page data prefetch and assign it to the root node
-          if (pages && pages[0] && pages[0].data_prefetch) {
-            copyManifestJSON.data_prefetch = pages[0].data_prefetch;
-            delete pages[0].data_prefetch;
-          }
-
-          copyManifestJSON = setRealUrlToManifest({
-            urlPrefix: pagePrefix,
-            urlSuffix: pageSuffix,
-            cdnPrefix,
-            isTemplate,
-            inlineStyle,
-            api,
-          }, copyManifestJSON);
-
-          compilation.assets[`${entryName}-manifest.json`] = new RawSource(JSON.stringify(copyManifestJSON, null, 2));
-
-          devUrls.push(`${cdnPrefix}${entryName}-manifest.json?pha=true`);
-        });
       }
 
       if (isStart) {
