58b5d80262f402fad8e481f16b7f8cbeb4677dbe
fushen <fushen.jzw@alibaba-inc.com>
2021-05-31 11:32:21 +0800

    fix: console dev url

--- packages/plugin-rax-app/src/utils/setDevUrlPrefix.js
+++ packages/plugin-rax-app/src/utils/setDevUrlPrefix.js
@@ -3,9 +3,9 @@ const { DEV_URL_PREFIX } = require('../constants');
 
 module.exports = (api) => {
   const { setValue, context } = api;
-  const { commandArgs } = context;
+  const { commandArgs, userConfig: { devServer: { host } } } = context;
   const protocol = commandArgs.https ? 'https' : 'http';
-  let urlPrefix = `${protocol}://${ address.ip() }:${ commandArgs.port }`;
+  let urlPrefix = `${protocol}://${ host || address.ip() }:${ commandArgs.port }`;
   if (process.env.CLOUDIDE_ENV) {
     urlPrefix = `https://${process.env.WORKSPACE_UUID}-${commandArgs.port}.${process.env.WORKSPACE_HOST}`;
   }
