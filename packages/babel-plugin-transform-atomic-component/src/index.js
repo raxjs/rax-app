28f34b6ac353a6d708e56d0a938d971082656ea2
fengzilong <fengzilong1992@gmail.com>
2021-06-17 11:13:21 +0800

    feat: pre-transpile rax-view and rax-text into native html tags

--- packages/babel-plugin-transform-atomic-component/src/index.js
+++ packages/babel-plugin-transform-atomic-component/src/index.js
@@ -1,10 +1,10 @@
 // Note: only detect `import x from 'rax-x'`, require is not supported
 
-const TO_RAX_VIEW_PROPS = 'toRaxViewProps'
-const TO_RAX_TEXT_PROPS = 'toRaxTextProps'
+const TO_RAX_VIEW_PROPS = 'toRaxViewProps';
+const TO_RAX_TEXT_PROPS = 'toRaxTextProps';
 
-module.exports = function ( { types: t, template } ) {
-  const buildToRaxTextProps = template( `
+module.exports = function ({ types: t, template }) {
+  const buildToRaxTextProps = template(`
     const transformProps = function (props) {
       const {
         className,
@@ -41,9 +41,9 @@ module.exports = function ( { types: t, template } ) {
         }
       };
     }
-  ` )
+  `);
 
-  const buildToRaxViewProps = template( `
+  const buildToRaxViewProps = template(`
     const transformProps = function (props) {
       const {
         className,
@@ -59,34 +59,34 @@ module.exports = function ( { types: t, template } ) {
         style: style
       };
     }
-  ` )
+  `);
 
   return {
     visitor: {
       Program: {
-        enter( path, state ) {
+        enter() {
           this._raxAtomicIds = new Map();
         },
-        exit(path, state) {
-          ;[
+        exit(path) {
+          [
             {
               id: TO_RAX_TEXT_PROPS,
-              init: buildToRaxTextProps().declarations[ 0 ].init,
+              init: buildToRaxTextProps().declarations[0].init,
             },
             {
               id: TO_RAX_VIEW_PROPS,
-              init: buildToRaxViewProps().declarations[ 0 ].init,
+              init: buildToRaxViewProps().declarations[0].init,
+            },
+          ].forEach(({ id, init }) => {
+            if (this._raxAtomicIds.has(id)) {
+              path.scope.push({
+                id: this._raxAtomicIds.get(id),
+                init,
+              });
             }
-          ].forEach( ( { id, init } ) => {
-            if ( this._raxAtomicIds.has( id ) ) {
-              path.scope.push( {
-                id: this._raxAtomicIds.get( id ),
-                init
-              } )
-            }
-          } )
+          });
 
-          this._raxAtomicIds = null
+          this._raxAtomicIds = null;
         },
       },
       JSXElement: {
@@ -101,10 +101,10 @@ module.exports = function ( { types: t, template } ) {
 
           const source = findImportSourceForJSXIdentifier(path, tagName);
 
-          if ( source === 'rax-view' ) {
-            transformRaxView.call( this, path, t )
-          } else if ( source === 'rax-text' ) {
-            transformRaxText.call( this, path, t )
+          if (source === 'rax-view') {
+            transformRaxView.call(this, path, t);
+          } else if (source === 'rax-text') {
+            transformRaxText.call(this, path, t);
           }
         },
       },
@@ -112,43 +112,43 @@ module.exports = function ( { types: t, template } ) {
   };
 };
 
-function addAtomicId( path, ids, id ) {
-  if ( !ids.has( id ) ) {
-    ids.set( id, generateIdentifierInProgramScope( path, id ) )
+function addAtomicId(path, ids, id) {
+  if (!ids.has(id)) {
+    ids.set(id, generateIdentifierInProgramScope(path, id));
   }
 }
 
-function generateIdentifierInProgramScope( path, id = '' ) {
-  const programScope = path.scope.getProgramParent()
-  return programScope.generateUidIdentifier( id )
+function generateIdentifierInProgramScope(path, id = '') {
+  const programScope = path.scope.getProgramParent();
+  return programScope.generateUidIdentifier(id);
 }
 
-function hasJSXSpreadAttribute( attributes = [] ) {
-  return attributes.some( attribute => {
-    return attribute.isJSXSpreadAttribute()
-  } )
+function hasJSXSpreadAttribute(attributes = []) {
+  return attributes.some((attribute) => {
+    return attribute.isJSXSpreadAttribute();
+  });
 }
 
-function removeAttributes( attributes, names = [] ) {
-  attributes.forEach( attribute => {
-    const name = getJSXAttributeName( attribute )
+function removeAttributes(attributes, names = []) {
+  attributes.forEach((attribute) => {
+    const name = getJSXAttributeName(attribute);
     if (
       attribute.isJSXAttribute() &&
-      names.includes( name )
+      names.includes(name)
     ) {
-      attribute.remove()
+      attribute.remove();
     }
-  } )
+  });
 }
 
-function spreadAttributes( path, t, id ) {
+function spreadAttributes(path, t, id) {
   // exclude x-、dangerouslySetInnerHTML
   const {
     object,
-    excludes
-  } = jsxAttributesToObjectExpression( path, t, {
-    exclude: excludeProp
-  } )
+    excludes,
+  } = jsxAttributesToObjectExpression(path, t, {
+    exclude: excludeProp,
+  });
 
   path.node.openingElement.attributes = [
     ...excludes,
@@ -156,181 +156,181 @@ function spreadAttributes( path, t, id ) {
       t.callExpression(
         id,
         [
-          object
-        ]
-      )
+          object,
+        ],
+      ),
     ),
-  ]
+  ];
 }
 
-function excludeProp( propName = '' ) {
-  return propName.startsWith( 'x-' ) ||
-    propName === 'dangerouslySetInnerHTML'
+function excludeProp(propName = '') {
+  return propName.startsWith('x-') ||
+    propName === 'dangerouslySetInnerHTML';
 }
 
-function jsxAttributesToObjectExpression( path, t, options = {} ) {
-  const { only, exclude } = options
+function jsxAttributesToObjectExpression(path, t, options = {}) {
+  const { only, exclude } = options;
 
-  const attributePaths = path.get( 'openingElement.attributes' )
+  const attributePaths = path.get('openingElement.attributes');
 
-  const excludes = []
+  const excludes = [];
 
-  const properties = attributePaths.map( attributePath => {
+  // eslint-disable-next-line array-callback-return
+  const properties = attributePaths.map((attributePath) => {
     // <el { ...expr }>
-    if ( attributePath.isJSXSpreadAttribute() ) {
+    if (attributePath.isJSXSpreadAttribute()) {
       return t.spreadElement(
-        attributePath.node.argument
-      )
+        attributePath.node.argument,
+      );
     }
 
-    if ( attributePath.isJSXAttribute() ) {
-      const name = getJSXAttributeName( attributePath )
-      const valuePath = attributePath.get( 'value' )
+    if (attributePath.isJSXAttribute()) {
+      const name = getJSXAttributeName(attributePath);
+      const valuePath = attributePath.get('value');
 
-      if ( only && ( only( name ) === false ) ) {
-        return
+      if (only && (only(name) === false)) {
+        return;
       }
 
-      if ( exclude && exclude( name ) ) {
-        excludes.push( attributePath.node )
-        return
+      if (exclude && exclude(name)) {
+        excludes.push(attributePath.node);
+        return;
       }
 
       // no value, e.g. <el attr />, use t.booleanLiteral(true)
       return t.objectProperty(
-        t.stringLiteral( name ),
+        t.stringLiteral(name),
         attributePath.node.value ?
-          getJSXAttributeValueExpression( valuePath ) :
-          t.booleanLiteral( true )
-      )
+          getJSXAttributeValueExpression(valuePath) :
+          t.booleanLiteral(true),
+      );
     }
 
-    excludes.push( attributePath.node )
-  } ).filter( Boolean )
+    excludes.push(attributePath.node);
+  }).filter(Boolean);
 
   return {
-    object: t.objectExpression( properties ),
+    object: t.objectExpression(properties),
     excludes,
+  };
+}
+
+function getJSXAttributeName(attributePath) {
+  if (!attributePath.isJSXAttribute()) {
+    return;
+  }
+
+  const namePath = attributePath.get('name');
+  if (namePath.isJSXIdentifier()) {
+    return namePath.node.name;
+  }
+
+  if (namePath.isJSXNamespacedName()) {
+    return `${ namePath.node.namespace.name }:${ namePath.node.name.name }`;
   }
 }
 
-function getJSXAttributeName( attributePath ) {
-  if ( !attributePath.isJSXAttribute() ) {
-    return
+function getJSXAttributeValueExpression(valuePath) {
+  if (valuePath.isJSXExpressionContainer()) {
+    return valuePath.node.expression;
   }
 
-  const namePath = attributePath.get( 'name' )
-  if ( namePath.isJSXIdentifier() ) {
-    return namePath.node.name
-  }
-
-  if ( namePath.isJSXNamespacedName() ) {
-    return `${ namePath.node.namespace.name }:${ namePath.node.name.name }`
-  }
-}
-
-function getJSXAttributeValueExpression( valuePath ) {
-  if ( valuePath.isJSXExpressionContainer() ) {
-    return valuePath.node.expression
-  }
-
-  return valuePath.node
+  return valuePath.node;
 }
 
 // rax-text start
-function transformRaxText( path, t ) {
-  changeTagName( path, 'span' )
+function transformRaxText(path, t) {
+  changeTagName(path, 'span');
 
-  const attributes = path.get( 'openingElement.attributes' )
+  const attributes = path.get('openingElement.attributes');
 
-  addAtomicId( path, this._raxAtomicIds, TO_RAX_TEXT_PROPS )
+  addAtomicId(path, this._raxAtomicIds, TO_RAX_TEXT_PROPS);
 
-  if ( attributes.length > 0 ) {
-    if ( hasJSXSpreadAttribute( attributes ) ) {
-      spreadAttributes( path, t, this._raxAtomicIds.get( TO_RAX_TEXT_PROPS ) )
+  if (attributes.length > 0) {
+    if (hasJSXSpreadAttribute(attributes)) {
+      spreadAttributes(path, t, this._raxAtomicIds.get(TO_RAX_TEXT_PROPS));
     } else {
       // TODO: find numberOfLines -> toRaxTextProps
-      const names = [ 'className', 'style', 'numberOfLines' ]
+      const names = ['className', 'style', 'numberOfLines'];
       const {
         object,
-      } = jsxAttributesToObjectExpression( path, t, {
-        only( name ) {
-          return names.includes( name )
+      } = jsxAttributesToObjectExpression(path, t, {
+        only(name) {
+          return names.includes(name);
         },
-      } )
+      });
 
-      removeAttributes( attributes, names )
+      removeAttributes(attributes, names);
 
       path.node.openingElement.attributes.unshift(
         t.jsxSpreadAttribute(
           t.callExpression(
-            this._raxAtomicIds.get( TO_RAX_TEXT_PROPS ),
+            this._raxAtomicIds.get(TO_RAX_TEXT_PROPS),
             [
-              object
-            ]
-          )
-        )
-      )
+              object,
+            ],
+          ),
+        ),
+      );
     }
   } else {
     // no attribute
     path.node.openingElement.attributes.unshift(
       t.jsxSpreadAttribute(
         t.callExpression(
-          this._raxAtomicIds.get( TO_RAX_TEXT_PROPS ),
+          this._raxAtomicIds.get(TO_RAX_TEXT_PROPS),
           [
-            t.ObjectExpression( [] )
-          ]
-        )
-      )
-    )
+            t.ObjectExpression([]),
+          ],
+        ),
+      ),
+    );
   }
 
-  const children = path.get( 'children' ) || []
+  const children = path.get('children') || [];
 
-  children.forEach( child => {
-    if ( child.isJSXElement() ) {
-      child.replaceWith( t.jsxText( `[object Object]` ) )
-      return
+  children.forEach((child) => {
+    if (child.isJSXElement()) {
+      child.replaceWith(t.jsxText('[object Object]'));
     }
-  } )
+  });
 }
 // rax-text end
 
 // rax-view start
-function transformRaxView( path, t ) {
-  changeTagName( path, 'div' )
+function transformRaxView(path, t) {
+  changeTagName(path, 'div');
 
-  const attributes = path.get( 'openingElement.attributes' )
+  const attributes = path.get('openingElement.attributes');
 
-  if ( attributes.length > 0 ) {
-    if ( hasJSXSpreadAttribute( attributes ) ) {
-      addAtomicId( path, this._raxAtomicIds, TO_RAX_VIEW_PROPS )
-      spreadAttributes( path, t, this._raxAtomicIds.get( TO_RAX_VIEW_PROPS ) )
+  if (attributes.length > 0) {
+    if (hasJSXSpreadAttribute(attributes)) {
+      addAtomicId(path, this._raxAtomicIds, TO_RAX_VIEW_PROPS);
+      spreadAttributes(path, t, this._raxAtomicIds.get(TO_RAX_VIEW_PROPS));
     } else {
-      prefixClassName( path, t, 'rax-view-v2' )
+      prefixClassName(path, t, 'rax-view-v2');
     }
   } else {
     // no attribute
-    prefixClassName( path, t, 'rax-view-v2' )
+    prefixClassName(path, t, 'rax-view-v2');
   }
 }
 
-function changeTagName( path, tagName = '' ) {
-  if ( !tagName ) {
-    return
+function changeTagName(path, tagName = '') {
+  if (!tagName) {
+    return;
   }
 
-  const openingElement = path.node.openingElement
-  const closeingElement = path.node.closingElement
+  const { openingElement } = path.node;
+  const closeingElement = path.node.closingElement;
 
-  openingElement.name.name = tagName
+  openingElement.name.name = tagName;
 
-  if ( openingElement.selfClosing === true ) {
-    return
+  if (openingElement.selfClosing === true) {
+    return;
   }
 
-  closeingElement.name.name = tagName
+  closeingElement.name.name = tagName;
 }
 
 // - cannot find attribute "className" -> className="prefixedClassName"
@@ -339,86 +339,86 @@ function changeTagName( path, tagName = '' ) {
 // - className="string" -> className="prefixedClassName string" }
 // - className={ expression } -> className={ 'prefixedClassName' + ( expression ) }
 // - others: warning
-function prefixClassName( path, t, className = '' ) {
-  const valuePath = findJSXAttributeValuePath( path, 'className' )
+function prefixClassName(path, t, className = '') {
+  const valuePath = findJSXAttributeValuePath(path, 'className');
 
-  if ( !valuePath ) {
+  if (!valuePath) {
     path.node.openingElement.attributes = [
       t.jsxAttribute(
-        t.jsxIdentifier( 'className' ),
-        t.stringLiteral( className )
-      )
-    ]
-    return
+        t.jsxIdentifier('className'),
+        t.stringLiteral(className),
+      ),
+    ];
+    return;
   }
 
-  if ( valuePath.isStringLiteral() ) {
+  if (valuePath.isStringLiteral()) {
     valuePath.replaceWith(
-      t.stringLiteral( className + ' ' + valuePath.node.value )
-    )
-  } else if ( valuePath.isJSXExpressionContainer() ) {
+      t.stringLiteral(`${className } ${ valuePath.node.value}`),
+    );
+  } else if (valuePath.isJSXExpressionContainer()) {
     valuePath.replaceWith(
       t.jsxExpressionContainer(
         t.binaryExpression(
           '+',
-          t.stringLiteral( className + ' ' ),
-          valuePath.node.expression
-        )
-      )
-    )
+          t.stringLiteral(`${className } `),
+          valuePath.node.expression,
+        ),
+      ),
+    );
   }
 }
 
-function findJSXAttributeValuePath( path, name ) {
-  const jsxAttributePath = findJSXAttributePath( path, name )
+function findJSXAttributeValuePath(path, name) {
+  const jsxAttributePath = findJSXAttributePath(path, name);
 
-  if ( !jsxAttributePath ) {
-    return
+  if (!jsxAttributePath) {
+    return;
   }
 
-  return jsxAttributePath.get( 'value' )
+  return jsxAttributePath.get('value');
 }
 
-function findJSXAttributePath( path, name ) {
-  const attributes = path.get( 'openingElement.attributes' ) || []
-  return attributes.find( attributePath => {
+function findJSXAttributePath(path, name) {
+  const attributes = path.get('openingElement.attributes') || [];
+  return attributes.find((attributePath) => {
     return attributePath.isJSXAttribute() &&
-      getJSXAttributeName(attributePath) === name
-  } )
+      getJSXAttributeName(attributePath) === name;
+  });
 }
 // rax-view end
 
 // 1. Got 'View'
 // 2. Find `import View from 'rax-view'` declaration by traversing parent scope
 // 3. Return 'rax-view'
-function findImportSourceForJSXIdentifier( path, identifierName ) {
-  const binding = findBindingInScope( path.scope, identifierName )
+function findImportSourceForJSXIdentifier(path, identifierName) {
+  const binding = findBindingInScope(path.scope, identifierName);
 
-  if ( !binding ) {
-    return
+  if (!binding) {
+    return;
   }
 
-  const bindingPath = binding.path
-  const bindingParentPath = bindingPath.parentPath
+  const bindingPath = binding.path;
+  const bindingParentPath = bindingPath.parentPath;
 
   if (
     bindingPath.isImportDefaultSpecifier() &&
     bindingPath.node.local.name === identifierName &&
     bindingParentPath.isImportDeclaration()
   ) {
-    return bindingParentPath.node.source.value
+    return bindingParentPath.node.source.value;
   }
 }
 
-function findBindingInScope( scope = {}, identifierName = '' ) {
-  const bindings = scope.bindings || {}
-  const binding = bindings[ identifierName ]
+function findBindingInScope(scope = {}, identifierName = '') {
+  const bindings = scope.bindings || {};
+  const binding = bindings[identifierName];
 
-  if ( binding ) {
-    return binding
+  if (binding) {
+    return binding;
   }
 
-  if ( scope.parent ) {
-    return findBindingInScope( scope.parent, identifierName )
+  if (scope.parent) {
+    return findBindingInScope(scope.parent, identifierName);
   }
 }
