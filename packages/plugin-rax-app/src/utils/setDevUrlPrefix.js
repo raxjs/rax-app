0f90628061fb79ae3aca61ccee30ce11f8eace16
fushen <fushen.jzw@alibaba-inc.com>
2021-06-22 14:59:28 +0800

    chore: dev url

--- packages/plugin-rax-app/src/utils/setDevUrlPrefix.js
+++ packages/plugin-rax-app/src/utils/setDevUrlPrefix.js
@@ -3,11 +3,11 @@ const { DEV_URL_PREFIX } = require('../constants');
 
 module.exports = (api) => {
   const { setValue, context } = api;
-  const { commandArgs, userConfig: { devServer: { host } } } = context;
+  const { commandArgs, userConfig: { devServer: { host, port } } } = context;
   const protocol = commandArgs.https ? 'https' : 'http';
-  let urlPrefix = `${protocol}://${ host || address.ip() }:${ commandArgs.port }`;
+  let urlPrefix = `${protocol}://${ commandArgs.host || host || address.ip() }:${ commandArgs.port }`;
   if (process.env.CLOUDIDE_ENV) {
-    urlPrefix = `https://${process.env.WORKSPACE_UUID}-${commandArgs.port}.${process.env.WORKSPACE_HOST}`;
+    urlPrefix = `https://${process.env.WORKSPACE_UUID}-${commandArgs.port || port}.${process.env.WORKSPACE_HOST}`;
   }
 
   setValue(DEV_URL_PREFIX, urlPrefix);
