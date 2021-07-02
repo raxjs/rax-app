65a501aa111513f35134e6f34b83e9639a311cb0
wjq990112 <1163585385@qq.com>
2021-07-03 00:41:31 +0800

    chore: change comments

--- packages/rax-webpack-config/src/setWebpackPlugins.ts
+++ packages/rax-webpack-config/src/setWebpackPlugins.ts
@@ -3,7 +3,7 @@ import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import * as FilterWarningsPlugin from 'webpack-filter-warnings-plugin';
 import * as CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
 
-// @ts-ignore
+// eslint-disable-next-line
 export default (config, options) => {
   config
     .plugin('MiniCssExtractPlugin')
