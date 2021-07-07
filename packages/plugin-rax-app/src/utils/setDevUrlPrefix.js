1b63b07690c727409dba77be8bb9786192d07da2
狒狒神 <fushen.jzw@alibaba-inc.com>
2021-07-07 20:44:25 +0800

    Update packages/plugin-rax-app/src/utils/setDevUrlPrefix.js
    Co-authored-by: 炽翎 <45777252+wjq990112@users.noreply.github.com>

--- packages/plugin-rax-app/src/utils/setDevUrlPrefix.js
+++ packages/plugin-rax-app/src/utils/setDevUrlPrefix.js
@@ -5,7 +5,7 @@ module.exports = (api) => {
   const { setValue, context } = api;
   const { commandArgs, userConfig: { devServer: { host, port } } } = context;
   const protocol = commandArgs.https ? 'https' : 'http';
-  let urlPrefix = `${protocol}://${ commandArgs.host || host || address.ip() }:${ commandArgs.port }`;
+  let urlPrefix = `${protocol}://${commandArgs.host || host || address.ip()}:${commandArgs.port}`;
   if (process.env.CLOUDIDE_ENV) {
     urlPrefix = `https://${process.env.WORKSPACE_UUID}-${commandArgs.port || port}.${process.env.WORKSPACE_HOST}`;
   }
