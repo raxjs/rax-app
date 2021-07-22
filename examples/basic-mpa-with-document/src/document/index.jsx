b91cfaff907a08b4c1a0d91318f472ab1c94288a
wjq990112 <1163585385@qq.com>
2021-07-22 15:12:27 +0800

    chore: revert example

--- examples/basic-mpa-with-document/src/document/index.jsx
+++ examples/basic-mpa-with-document/src/document/index.jsx
@@ -13,7 +13,6 @@ function Document(props) {
               name="viewport"
               content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover"
             />
-            <link rel="stylesheet" href={stylesheet} />
             <title>{ props.title }</title>
             <Style />
           </head>
