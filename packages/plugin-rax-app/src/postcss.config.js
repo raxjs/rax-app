6397e11113a11b19c7e9bab2a2f03dda6c1a31f8
炽翎 <45777252+wjq990112@users.noreply.github.com>
2021-08-11 16:16:10 +0800

    feat: merge postcss options (#799)
    * chore: update version
    
    * fix: mpa entries (#781)
    
    * fix: tabbar state update (#783)
    
    * fix: tabbar state update
    
    * chore: optimize code
    
    * chore: optimize code
    
    * chore: test cases
    
    * chore: optimize code
    
    * chore: optimize code
    
    * chore: optimize component name
    
    * chore: format code
    
    * feat: merge-postcss-options
    
    * feat: merge postcss options
    
    Co-authored-by: 狒狒神 <fushen.jzw@alibaba-inc.com>

--- packages/plugin-rax-app/src/postcss.config.js
+++ packages/plugin-rax-app/src/postcss.config.js
@@ -1,42 +1,5 @@
-/* eslint-disable no-case-declarations */
-/* eslint-disable global-require */
-const atImport = require('postcss-import');
+const defaultPostcssConfig = require('./defaultPostcssConfig');
 
-// See https://github.com/postcss/postcss-loader#context-ctx
-module.exports = ({ options }) => {
-  const type = options && options.type;
-  return {
-    plugins: getPlugins(type),
-  };
-};
-
-function getPlugins(type) {
-  switch (type) {
-    case 'normal':
-      return [
-        atImport(),
-      ];
-    // Inline style
-    case 'web-inline':
-      return [
-        atImport(),
-        require('postcss-plugin-rpx2vw')(),
-      ];
-
-    // extract css file in web while inlineStyle is disabled
-    // web standard
-    case 'web':
-      return [
-        atImport(),
-        require('postcss-preset-env')({
-          autoprefixer: {
-            flexbox: 'no-2009',
-          },
-          stage: 3,
-        }),
-        require('postcss-plugin-rpx2vw')(),
-      ];
-    default:
-      return [];
-  }
-}
+module.exports = ({ options }) => ({
+  ...defaultPostcssConfig(options),
+});
