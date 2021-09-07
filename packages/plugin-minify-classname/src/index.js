b7dd20ecd7810b9f2fbf8879da7622d91167729d
MO <9125255+fengzilong@users.noreply.github.com>
2021-09-07 17:25:40 +0800

    style: add semi

--- packages/plugin-minify-classname/src/index.js
+++ packages/plugin-minify-classname/src/index.js
@@ -11,7 +11,7 @@ module.exports = function minifyCSSModulesClassname({onGetWebpackConfig, context
       configCSSModulesOptions(config, {
         getLocalIdent
       });
-    })
+    });
   }
 
   // cache readPkgUp result
