f3d5c82c6e61cd0f1c177f4a0709a023feca20f7
山河 <406400939@qq.com>
2021-10-11 11:41:31 +0800

    【Enhance】Fusion Mobile 包体积优化 (#841)
    * feat: add build-plugin-fusion-mobile
    
    * chore: modify version & entry
    
    * chore: optimize code
    
    * chore: optimize code
    
    * feat(plugin-fusion-mobile): support transform css vars into static values
    
    * chore(plugin-fusion-mobile): optimize print info
    
    * docs: add changelog
    
    * chore: code optimizing
    
    * docs: add chinese reamde
    
    * docs: code optimizing
    
    * feat: add themePackage config
    
    * chore: log optimizing
    
    * chore: revert pkg version
    
    * docs: update reamde
    
    * Update packages/plugin-fusion-mobile/README.md
    
    Co-authored-by: 炽翎 <45777252+wjq990112@users.noreply.github.com>
    
    * docs: update readme doc
    
    * refactor: postcss api update
    
    * chore: use build-script log
    
    * docs: update doc
    
    * chore: update version of plugin-fusion-mobile
    
    Co-authored-by: 联民 <lianmin.slm@alibaba-inc.com>
    Co-authored-by: 狒狒神 <fushen.jzw@alibaba-inc.com>
    Co-authored-by: 狒狒神 <350740249@qq.com>
    Co-authored-by: 炽翎 <45777252+wjq990112@users.noreply.github.com>

---
+++ packages/plugin-fusion-mobile/src/utils/removeCssVar.js
@@ -0,0 +1,36 @@
+const postcss = require('postcss');
+const fs = require('fs-extra');
+const CleanCSS = require('clean-css');
+const page2root = require('../css-plugins/replaceRoot');
+const removeInvalidDecls = require('../css-plugins/removeInvalidDecls');
+
+module.exports = (filePath, themePath, callback) => {
+  const cssContents = fs.readFileSync(filePath, {
+    encoding: 'utf8',
+  });
+
+  let themeContents = '';
+
+  if (themePath && fs.existsSync(themePath)) {
+    themeContents = fs.readFileSync(themePath, 'utf8');
+  }
+
+  (async () => {
+    const result = await postcss([
+      require('postcss-import'),
+      // transform 'page {}' selectors into ':root {}' for wechat-miniprogram in order to remove css vars
+      page2root,
+      require('postcss-css-variables'),
+      removeInvalidDecls,
+    ]).process(`${cssContents} \n ${themeContents}`, {});
+
+    const r = new CleanCSS({
+      level: 2,
+      inline: ['all'],
+    }).minify(result.css);
+
+    fs.writeFileSync(filePath, r.styles);
+
+    callback();
+  })();
+};
