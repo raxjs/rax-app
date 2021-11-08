6b3cfbf8730b41a63cb5474a5e67e7f88c0a1758
wjq990112 <1163585385@qq.com>
2021-11-08 17:01:27 +0800

    chore: add comment

--- packages/plugin-rax-web/src/setDev.ts
+++ packages/plugin-rax-web/src/setDev.ts
@@ -38,7 +38,9 @@ export default (config) => {
 
     app.get(/^\/?(?!\.(js|css|json))/, (req, res, next) => {
       // `.html` in search params should not match
-      const path = new URL(req.url, 'https://placeholder.com').pathname;
+      // `http://localhost` is a placeholder in `URL`, if remove, it will throw error
+      // eslint-disable-next-line @iceworks/best-practices/no-http-url
+      const path = new URL(req.url, 'http://localhost').pathname;
       const matched = path.match(/^\/(\S*?)\.html/);
       if (!matched) {
         next();
