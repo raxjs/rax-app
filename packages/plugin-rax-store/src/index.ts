0d269317c2f95ed97ce360443c5f667bead2d3d1
狒狒神 <fushen.jzw@alibaba-inc.com>
2021-11-09 19:08:35 +0800

    Fix/test case (#887)
    * fix: ts warning with type
    
    * chore: revert useless change
    
    * chore: test cases

--- packages/plugin-rax-store/src/index.ts
+++ packages/plugin-rax-store/src/index.ts
@@ -64,11 +64,19 @@ export default async (api) => {
         mpa,
       });
 
+    let raxPath;
+
+    try {
+      raxPath = require.resolve(path.join(rootDir, 'node_modules', 'rax/lib/compat'));
+    } catch (e) {
+      raxPath = 'rax/lib/compat';
+    }
+
     // Set alias to run @ice/store
     config.resolve.alias
       .set('$store', fse.pathExistsSync(appStorePath) ? appStorePath : path.join(tempPath, 'plugins', 'store', 'index.ts'))
       .set('react-redux', require.resolve('rax-redux'))
-      .set('react', path.join(rootDir, 'node_modules', 'rax/lib/compat'));
+      .set('react', raxPath);
   });
 
   const gen = new CodeGenerator({
