52bd7fd505b47a4c940fc59a0828f0de60c13504
fushen <fushen.jzw@alibaba-inc.com>
2021-12-16 17:42:08 +0800

    chore: ci

--- packages/plugin-ssr/src/ssr/addBuiltInRenderComponentToHTML.ts
+++ packages/plugin-ssr/src/ssr/addBuiltInRenderComponentToHTML.ts
@@ -4,11 +4,7 @@ import addPageHTMLAssign from './addPageHTMLAssign';
 export default function addBuiltInRenderComponentToHTML({ updateDataInClient }: IFormattedLoaderQuery) {
   return `
   async function renderComponentToHTML(Component, ctx, initialData, htmlTemplate) {
-    if (!htmlTemplate) {
-      htmlTemplate = '__RAX_APP_SERVER_HTML_TEMPLATE__';
-    }
-
-    const $ = new Generator(htmlTemplate);
+    const $ = new Generator(htmlTemplate || '__RAX_APP_SERVER_HTML_TEMPLATE__');
 
     const pageInitialProps = await getInitialProps(Component, ctx);
     const data = {
