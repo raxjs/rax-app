df2f45a958679d727c87265fec8fe347571738e6
fushen <fushen.jzw@alibaba-inc.com>
2022-01-05 17:46:38 +0800

    chore: add ssr renderToHTML test case

--- __tests__/build-cases/basic-spa-ssr.test.ts
+++ __tests__/build-cases/basic-spa-ssr.test.ts
@@ -6,6 +6,11 @@ const example = 'basic-spa-ssr';
 const BUNDLE_URL = require.resolve(path.join(process.cwd(), `examples/${example}/build/node/index.js`));
 
 describe('should build web result: ', () => {
+  beforeEach(() => {
+    // Reset all require cache
+    jest.resetModules()
+  });
+
   buildFixture(example);
 
   test('ssr render method', async () => {
@@ -24,6 +29,19 @@ describe('should build web result: ', () => {
     };
     const ctx = { req: ctxReq, res: ctxRes };
     await render(ctx);
+    const $ = cheerio.load(html, { decodeEntities: false });
+    expect($('.title').text()).toBe('Welcome to Your Rax App with SSR');
+  });
+
+  test('ssr renderToHTML method', async () => {
+    const { renderToHTML } = require(BUNDLE_URL);
+    const req = {
+      url: '/?a=1',
+      path: '/',
+    };
+
+    const res = {};
+    const html  = await renderToHTML(req, res);
     const $ = cheerio.load(html, { decodeEntities: false });
     expect($('.title').text()).toBe('Welcome to Your Rax App with SSR');
   });
