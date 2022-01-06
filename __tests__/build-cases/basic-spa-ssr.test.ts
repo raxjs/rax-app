8ba6f12441d63c60f32e643a0fc3a99b69d4759e
狒狒神 <fushen.jzw@alibaba-inc.com>
2022-01-06 11:42:31 +0800

    chore: add ssr renderToHTML test case (#948)
    * chore: add ssr renderToHTML test case
    
    * chore: ci

--- __tests__/build-cases/basic-spa-ssr.test.ts
+++ __tests__/build-cases/basic-spa-ssr.test.ts
@@ -3,13 +3,17 @@ import * as cheerio from 'cheerio';
 import { buildFixture } from '../utils/build';
 
 const example = 'basic-spa-ssr';
-const BUNDLE_URL = require.resolve(path.join(process.cwd(), `examples/${example}/build/node/index.js`));
 
 describe('should build web result: ', () => {
+  beforeEach(() => {
+    // Reset all require cache
+    jest.resetModules()
+  });
+
   buildFixture(example);
 
   test('ssr render method', async () => {
-    const { render } = require(BUNDLE_URL);
+    const { render } = require(path.join(process.cwd(), `examples/${example}/build/node/index.js`));
     const ctxReq = {
       url: '/?a=1',
       path: '/',
@@ -24,6 +28,19 @@ describe('should build web result: ', () => {
     };
     const ctx = { req: ctxReq, res: ctxRes };
     await render(ctx);
+    const $ = cheerio.load(html, { decodeEntities: false });
+    expect($('.title').text()).toBe('Welcome to Your Rax App with SSR');
+  });
+
+  test('ssr renderToHTML method', async () => {
+    const { renderToHTML } = require(path.join(process.cwd(), `examples/${example}/build/node/index.js`));
+    const req = {
+      url: '/?a=1',
+      path: '/',
+    };
+
+    const res = {};
+    const html  = await renderToHTML(req, res);
     const $ = cheerio.load(html, { decodeEntities: false });
     expect($('.title').text()).toBe('Welcome to Your Rax App with SSR');
   });
