6dffd4b325534f98161e4b7ef335d11b28af2a4d
wjq990112 <1163585385@qq.com>
2022-01-18 20:11:49 +0800

    feat: migrate rpx2vw to postcss 8 & lock postcss version globally

--- packages/postcss-plugin-rpx2vw/src/index.js
+++ packages/postcss-plugin-rpx2vw/src/index.js
@@ -1,4 +1,3 @@
-const postcss = require('postcss');
 // !singlequotes|!doublequotes|!url()|pixelunit
 const rpxRegex = /"[^"]+"|'[^']+'|url\([^\)]+\)|(\d*\.?\d+)rpx/g;
 
@@ -9,31 +8,35 @@ const defaults = {
   unitPrecision: 5,
 };
 
-module.exports = postcss.plugin('postcss-rpx2vw', (options) => {
+// migration guide: https://evilmartians.com/chronicles/postcss-8-plugin-migration
+module.exports.postcss = true;
+
+module.exports = (options) => {
   const opts = Object.assign({}, defaults, options);
 
-  return function (root) {
-    root.walkDecls((decl) => {
-      // This should be the fastest test and will remove most declarations
+  return {
+    postcssPlugin: 'postcss-plugin-rpx2vw',
+    Declaration(decl) {
       if (decl.value.indexOf('rpx') === -1) return;
 
       const unit = getUnit(decl.prop, opts);
       decl.value = decl.value.replace(rpxRegex, createRpxReplace(opts, unit, opts.viewportWidth));
-    });
+    },
+    AtRule: {
+      media: (rule) => {
+        if (rule.params.indexOf('rpx') === -1) return;
 
-    root.walkAtRules('media', (rule) => {
-      if (rule.params.indexOf('rpx') === -1) return;
-
-      rule.params = rule.params.replace(rpxRegex, createRpxReplace(opts, opts.viewportUnit, opts.viewportWidth));
-    });
+        rule.params = rule.params.replace(rpxRegex, createRpxReplace(opts, opts.viewportUnit, opts.viewportWidth));
+      },
+    },
   };
-});
+};
 
 function toFixed(number, precision) {
   const multiplier = Math.pow(10, precision + 1);
   const wholeNumber = Math.floor(number * multiplier);
 
-  return Math.round(wholeNumber / 10) * 10 / multiplier;
+  return (Math.round(wholeNumber / 10) * 10) / multiplier;
 }
 
 // transform rpx to vw
@@ -41,7 +44,7 @@ function createRpxReplace(opts, viewportUnit, viewportSize) {
   return function (m, $1) {
     if (!$1) return m;
     const pixels = parseFloat($1);
-    const parsedVal = toFixed(pixels / viewportSize * 100, opts.unitPrecision);
+    const parsedVal = toFixed((pixels / viewportSize) * 100, opts.unitPrecision);
     return parsedVal + viewportUnit;
   };
 }
