9dbafa46cc81c15537e318702bdf104d1b821359
fushen <fushen.jzw@alibaba-inc.com>
2022-02-08 14:30:04 +0800

    fix: type error

--- packages/plugin-rax-app/src/generateTplFile/index.js
+++ packages/plugin-rax-app/src/generateTplFile/index.js
@@ -2,5 +2,4 @@ const path = require('path');
 
 module.exports = (applyMethod) => {
   applyMethod('addPluginTemplate', path.join(__dirname, 'templates'));
-  applyMethod('addAppConfigTypes', { exportName: 'renderComponent?: FrameworkComponentType' });
 };
