4bcc5daa2e003e46aa7d13097bbfde78c19afd18
狒狒神 <fushen.jzw@alibaba-inc.com>
2022-03-31 19:22:46 +0800

    fix: only inject webpack hot client in webpack4 (#986)
    * fix: only inject webpack hot client in webpack4
    
    * chore: open error overlay

--- packages/plugin-rax-app/src/utils/injectHotReloadEntries.ts
+++ packages/plugin-rax-app/src/utils/injectHotReloadEntries.ts
@@ -1,11 +1,15 @@
+import { isWebpack4 } from '@builder/compat-webpack4';
+
 export default function (config) {
   const entries = config.entryPoints.entries();
   Object.keys(entries).forEach((entryName) => {
     const entrySet = config.entry(entryName);
     const entryFiles = entrySet.values();
     const finalEntryFile = entryFiles[entryFiles.length - 1];
-    // Add webpack hot dev client
-    entrySet.prepend(require.resolve('react-dev-utils/webpackHotDevClient'));
+    // Add webpack hot dev client for webpack4
+    if (isWebpack4) {
+      entrySet.prepend(require.resolve('react-dev-utils/webpackHotDevClient'));
+    }
     // Add module.hot.accept() to entry
     entrySet.add(`${require.resolve('../Loaders/HmrLoader')}!${finalEntryFile}`);
     entrySet.delete(finalEntryFile);
