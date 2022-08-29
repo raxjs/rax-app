546c3c63a29a082fe160cefcd47adc3fabcfa334
answershuto <answershuto@gmail.com>
2022-08-29 15:17:46 +0800

    test: modify manifest test

--- __tests__/build-cases/with-rax-pha.test.ts
+++ __tests__/build-cases/with-rax-pha.test.ts
@@ -9,24 +9,25 @@ buildFixture(example, true);
 describe('should build manifest.json: ', () => {
   test('manifest.json content', async () => {
     const manifestJSON = fs.readJSONSync(path.join(process.cwd(), 'examples', example, 'build/web/manifest.json'));
+    console.log('manifestJSON=', manifestJSON)
     expect(manifestJSON).toEqual({
       app_worker: { url: 'pha-worker.js' },
       links: [],
       metas: ['<meta name="release-info" content="version=12,app-id=123" />'],
       pages: [
         {
-          frames: [
-            { name: 'home', path: '/', source: 'pages/Home/index', title: 'home', url: 'https://m.taobao.com' },
-            { name: 'about', path: '/about', source: 'pages/About/index' },
-          ],
-          tab_header: { height: 150, source: 'pages/Header/index' },
-        },
-        {
           name: 'about-single',
           path: '/about-single',
           source: 'pages/About-Single/index',
           url: 'https://m.taobao.com',
         },
+        {
+          frames: [
+            { name: 'home', path: '/', source: 'pages/Home/index', title: 'home', url: 'https://m.taobao.com' },
+            { name: 'about', path: '/about', source: 'pages/About/index' },
+          ],
+          tab_header: { height: 150, source: 'pages/Header/index' },
+        }
       ],
       scripts: [
         '<script src="https://g.alicdn.com/mtb/lib-promise/3.1.3/polyfillB.js"  crossorigin="anonymous" ></script>',
