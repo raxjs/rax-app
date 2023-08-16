6c86dba0391aec9240341d3f9a6f168fe36078fc
shiftj18 <stjkl@foxmail.com>
2023-08-16 20:07:12 +0800

    fix(rax-app): debug()  run as needed and optimizing stringify (#1029)
    * fix(rax-app): debug()  run as needed and replace JSON.stringify with util.inspect for circular
    
    * docs(rax-app): change version, readme and comment

--- packages/plugin-rax-app/src/utils/logWebpackConfig.ts
+++ packages/plugin-rax-app/src/utils/logWebpackConfig.ts
@@ -1,19 +1,15 @@
 import debugCore from 'debug';
+import { inspect } from 'util';
 
 const debug = debugCore('rax-app');
 
 export default (configs) => {
   try {
-    const tmp = [];
-    debug(JSON.stringify(configs, (key, val) => {
-      if (val != null && typeof val === 'object') {
-        if (tmp.indexOf(val) >= 0) {
-          return;
-        }
-        tmp.push(val);
-      }
-      return val;
-    }, 2));
+    // Only executed when the debug mode is turned on
+    if (debug.enabled) {
+      // Print webpack configs for easy debugging
+      debug(inspect(configs, { depth: 10, colors: true }));
+    }
   } catch (error) {
     // ignore error
   }
